125966353ee4065558a803c9e6f404a3bbfdcec9
Alfonsus Avianto Chandrawan <alfonsus.chandrawan@traveloka.com>
2024-11-07 18:08:21 +0700

    add shared lib

---
+++ src/lib/notification.ts
@@ -0,0 +1,3 @@
+export function showNotification({ title, message }: { title: string, message: string; }) {
+  chrome.notifications.create('notification', { type: 'basic', iconUrl: "icons/icon-128x128.png", title, message });
+}
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./globals.css";
 import { Button } from "@/components/ui/button";
+import { showNotification } from "@/lib/notification";
 
 const Popup: React.FC = () => (
-  <div className="w-80 h-80 shadow">
-    Hello from React TypeScript Popup!
-    <Button>Hello World</Button>
+  <div className="w-80 h-80 shadow flex flex-col items-center justify-center">
+    <p className="text-lg font-bold mb-4">Hello from React TypeScript Popup!</p>
+    <Button
+      onClick={() => {
+        showNotification({
+          title: "Installed",
+          message: "Extension Installed",
+        });
+      }}
+    >
+      Hello World
+    </Button>
   </div>
 );
 
