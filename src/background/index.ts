848e30383b9c0d2a254114f60f3d06c07df792aa
Alfonsus Avianto Chandrawan <alfonsus.chandrawan@traveloka.com>
2024-12-11 21:00:43 +0700

    update console log

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,6 @@
 import { showNotification } from "@/lib/notification";
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Chrome Extension Installed');
+  console.log('Chrome Extension Installed Update');
   showNotification({ title: 'Installed', message: 'Installed Successfully' });
 });
